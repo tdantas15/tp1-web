0ec03f52c79208c8b54e92b7b8e2302d02749fc8
Talles Dantas <tdantas@avenuecode.com>
2015-05-07 21:41:40 -0300

    Adicionando busca por localização e temperatura

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 // Retorna a temperatura atual de Belo Horizonte
-function temperaturaBh(){
+function atualizarTela(){
 	var temperatura, resposta;
 	var r = new XMLHttpRequest();
 	var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D455821&format=json"
@@ -17,4 +17,19 @@ function temperaturaBh(){
 
 function convertToC(temperatura) {
     return (temperatura - 32) * (5 / 9);
+}
+
+function buscarTemperatura(latitude,longitude){
+	var resposta, woeid;
+	var r = new XMLHttpRequest();
+	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22"+latitude+"%2C%20"+longitude+"%22%20and%20gflags%3D%22R%22&appid=test"
+
+	r.open("GET", url, true);
+	r.onreadystatechange = function () {
+  		if (r.readyState != 4 || r.status != 200) return null;
+  		resposta = JSON.parse(r.responseText);
+  		temperatura = convertToC(resposta.query.results.channel.item.condition.temp);
+  		return temperatura;
+	};
+	r.send();
 }
