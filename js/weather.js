ee16a3bde744fe5073c68b163fbefd96b3184424
Talles Dantas <tdantas@avenuecode.com>
2015-05-07 21:46:31 -0300

    Adicionando busca por localização e temperatura - renomeando metodos

--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,5 @@
-// Retorna a temperatura atual de Belo Horizonte
-function atualizarTela(){
-	var temperatura, resposta;
-	var r = new XMLHttpRequest();
-	var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D455821&format=json"
 
-
-	r.open("GET", url, true);
-	r.onreadystatechange = function () {
-  		if (r.readyState != 4 || r.status != 200) return null;
-  		resposta = JSON.parse(r.responseText);
-  		temperatura = convertToC(resposta.query.results.channel.item.condition.temp);
-  		return temperatura;
-	};
-	r.send();
-}
-
-function convertToC(temperatura) {
-    return (temperatura - 32) * (5 / 9);
-}
-
-function buscarTemperatura(latitude,longitude){
+function buscarLocalizacao(latitude,longitude){
 	var resposta, woeid;
 	var r = new XMLHttpRequest();
 	var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22"+latitude+"%2C%20"+longitude+"%22%20and%20gflags%3D%22R%22&appid=test"
@@ -27,9 +7,34 @@ function buscarTemperatura(latitude,longitude){
 	r.open("GET", url, true);
 	r.onreadystatechange = function () {
   		if (r.readyState != 4 || r.status != 200) return null;
-  		resposta = JSON.parse(r.responseText);
-  		temperatura = convertToC(resposta.query.results.channel.item.condition.temp);
-  		return temperatura;
+  		woeid = 65165165;
+  		buscarTemperatura(woeid);
 	};
 	r.send();
+}
+
+function buscarTemperatura(woeid){
+	var temperatura, resposta;
+	var r = new XMLHttpRequest();
+	var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D"+woeid+"&format=json";
+
+
+	r.open("GET", url, true);
+	r.onreadystatechange = function () {
+  		if (r.readyState != 4 || r.status != 200) return null;
+  		resposta = JSON.parse(r.responseText);
+  		temperatura = converterParaCelsius(resposta.query.results.channel.item.condition.temp);
+  		atualizarTela(temperatura);
+	};
+	r.send();
+}
+
+function converterParaCelsius(temperatura) {
+    return (temperatura - 32) * (5 / 9);
+}
+
+
+
+function atualizarTela(temperatura){
+	alert(temperatura);
 }
