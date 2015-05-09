830762ac47781d3b53b3ed99decf6482bb9ff72f
Talles Dantas <tallesrsdantas@gmail.com>
2015-05-09 12:23:14 -0300

    Pagina de projetos criada

---
+++ js/projetos.js
@@ -0,0 +1,23 @@
+document.onreadystatechange = function() {
+	var div;
+	var imagens = document.getElementsByClassName("img-projeto");
+	for ( var i=0; i< imagens.length; i++ ){
+		imagens[i].addEventListener('click', function (){
+			if (this.width == '' || this.width == 200){
+				this.parentNode.classList.remove("col-md-3");
+				this.parentNode.classList.add("col-md-6");
+				div = this.parentNode.querySelector('.col-md-9');
+				div.classList.remove("col-md-9");
+				div.classList.add("col-md-6");
+				this.width = 400;
+			} else {
+				this.parentNode.classList.remove("col-md-6");
+				this.parentNode.classList.add("col-md-3");
+				div = this.parentNode.querySelector('.col-md-6');
+				div.classList.remove("col-md-6");
+				div.classList.add("col-md-9");
+				this.width = 200;
+			}
+		});
+	}
+	}
